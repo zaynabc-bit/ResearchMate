1c315c474ef5240d24d8ac827eef627ea559a6e8
zaynabc-bit <zaynabc-bit@users.noreply.github.com>
2026-07-01 18:25:52 +0100

    Fix import error in models/__init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,6 @@
 from .folder import Folder
 from .paper import ResearchPaper
 from .chunk import PaperChunk
-from .chat import ChatMessage, DBChatMessage, GlobalChatMessage
+from .chat import ChatMessage
 from .comparison import PaperComparison
 from .rewrite import SavedRewrite
